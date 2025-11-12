0586d01a7399e77b046b88c7bb6789a276835c99
Caleb Poock <caleb.poock@gmail.com>
2025-11-12 13:06:45 -0500

    train_model.py run, generated model/encoder.pkl, model/model.pkl, and slice_output.txt

--- ml/model.py
+++ ml/model.py
@@ -125,7 +125,7 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        data = data[data[column_name] == slice_value],
+        X = data[data[column_name] == slice_value],
         categorical_features=categorical_features,
         label=label,
         training=False,
