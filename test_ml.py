e6fe41c044398194ad957fc2a4cd45013c646f07
Caleb Poock <caleb.poock@gmail.com>
2025-11-12 14:11:05 -0500

    Unit tests implemented in test_ml.py; all tests passed; screenshot of console output for passed tests added as screenshots/unit_test.png

--- test_ml.py
+++ test_ml.py
@@ -2,30 +2,63 @@ import pytest
 import pandas as pd
 import numpy as np
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+
+from ml.model import train_model, compute_model_metrics
+from train_model import cat_features
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_algorithm_type():
     """
-    # add description for the first test
+    Tests that the model is a RandomForestClassifier
     """
-    # Your code here
-    pass
+
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 0, 1, 1])
+
+    model = train_model(X, y)
+    assert isinstance(model, RandomForestClassifier), "Model is not a RandomForestClassifier"
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_metrics():
     """
-    # add description for the second test
+    Tests that the compute_model_metrics function returns the correct precision, recall, and F-beta scores.
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 0, 1, 1])
+
+    model = train_model(X, y)
+
+    preds = model.predict(X)
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert fbeta == fbeta_score(y, preds, beta=1, zero_division=1), "F-beta score does not match expected value"
+    assert recall == recall_score(y, preds, zero_division=1), "Recall does not match expected value"
+    assert precision == precision_score(y, preds, zero_division=1), "Precision does not match expected value"
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_categorical_features_list():
     """
-    # add description for the third test
+    Test that the list of categorical features passed in `train_model.py` is correct and contains the expected features.
     """
     # Your code here
-    pass
+    assert isinstance(cat_features, list), "cat_features in train_model.py should be a list"
+    expected_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country"
+    ]
+    for feature in expected_features:
+        assert feature in cat_features, f"{feature} is missing from cat_features list in train_model.py"
